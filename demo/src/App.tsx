33a5b177da851282d8b6e67c65d200cca93fb0fb
lzhao <zhao434@usc.edu>
2018-02-08 19:09:35 -0800

    update snippet

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,15 +1,39 @@
 import * as React from 'react';
-// import './App.css';
 import * as styles from './App.css';
 import Person from './Person/Person';
 
-class App extends React.Component {
+interface AppState {
+  persons: { name: string, age: number }[];
+}
+
+interface AppProps {
+  title?: string;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  static defaultProps: AppProps = {
+    title: 'demo'
+  };
+
+  state: AppState = {
+    persons: [
+      { name: 'Max11', age: 28 },
+      { name: 'Lei', age: 26 },
+      { name: 'Manu', age: 29 },
+    ]
+  };
+
   render() {
     return (
       <div className={styles.App} >
-        <h1>Hi, I'm a React App</h1>
+        <h1>Hi, This is {this.props.title} App</h1>
         <p>This is really working!</p>
-        <Person/>
+        <button>Switch Name</button>
+        {this.state.persons.map((person, index) => {
+          return <Person name={person.name} age={person.age} key={index} />;
+        })}
+        <Person />
       </div>
     );
   }
--- demo/src/Person/Person.tsx
+++ demo/src/Person/Person.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import { ReactNode, StatelessComponent } from 'react';
 
-const Person = () => {
+interface PersonProps {
+  children?: ReactNode;
+  name?: string;
+  age?: number;
+}
+
+const Person: StatelessComponent<PersonProps> = (props: PersonProps) => {
   return (
-    <p>I'm a Person!</p>
+    <div>
+      <p>I'm {props.name} and I'm {props.age} </p>
+      <p>{props.children}</p>
+    </div>
   );
+};
+
+Person.defaultProps = {
+  name: 'Lei',
+  age: 28,
 };
 
 export default Person;
