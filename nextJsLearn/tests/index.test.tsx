cda1d758fc9a679ecba4d3efd637aafbec6a8cab
Lei Zhao <lei.zhao@coupa.com>
2020-03-26 08:00:18 -0700

    add by auto checkIn

---
+++ nextJsLearn/tests/index.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../src/pages';
+import { render } from '@testing-library/react';
+
+describe('HomePage', () => {
+  const getComponent = () => render(<Home shows={[{ id: 1, show: { name: 'show' } }]} />);
+
+  it('should render Home Page', () => {
+    const { container } = getComponent();
+    expect(container).not.toBeNull();
+  });
+});
