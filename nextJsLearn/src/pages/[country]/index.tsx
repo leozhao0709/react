457ad905e01afa5b006eb83ade688dfaf8ecbc3b
Lei Zhao <lei.zhao@coupa.com>
2020-03-27 18:41:28 -0700

    add by auto checkIn

--- nextJsLearn/src/pages/[country]/index.tsx
+++ nextJsLearn/src/pages/[country]/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { NextPage } from 'next';
+import React from 'react';
+import { NextPage, GetServerSideProps, GetStaticPaths } from 'next';
 import axios from 'axios';
-// import { Button } from '@leozhao0709/react-millhouse';
 
 interface HomeProps extends React.HTMLAttributes<HTMLDivElement> {
   shows: any[];
@@ -17,25 +16,27 @@ const Home: NextPage<HomeProps> = (props: HomeProps) => {
   const { shows } = props;
   const renderShows = () => shows.map(showItem => <li key={showItem.id}>{showItem.show.name}</li>);
 
-  let AddButton;
-  const renderAddButton = () => {
-    if (typeof window !== 'undefined') {
-      AddButton = import('@leozhao0709/react-millhouse').then(mod => mod.AddButton);
-    }
-
-    return AddButton;
-  };
-
-  return (
-    <ul>
-      {renderShows()}
-      {renderAddButton()}
-    </ul>
-  );
+  return <ul>{renderShows()}</ul>;
 };
 
-export const getStaticProps = async () => {
-  const res = await axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01');
+// export const getStaticProps = async () => {
+//   const res = await axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01');
+//   const shows = res.data;
+
+//   return {
+//     props: {
+//       shows
+//     }
+//   };
+// };
+
+// export const getStaticPath = async () => {
+
+// }
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const { params } = ctx;
+  const res = await axios.get(`http://api.tvmaze.com/schedule?country=${params!.country}&date=2014-12-01`);
   const shows = res.data;
 
   return {
