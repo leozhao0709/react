d86f8eee8d67888040af50c68ad64d376600c1b0
Lei Zhao <lei.zhao@coupa.com>
2020-04-03 18:26:27 -0700

    add by auto checkIn

--- nextJsLearn/src/pages/_app.tsx
+++ nextJsLearn/src/pages/_app.tsx
@@ -12,11 +12,7 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
