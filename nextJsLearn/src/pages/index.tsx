0801cf182ed322610cf29d4c6892bf984f9da3b5
Lei Zhao <lei.zhao@coupa.com>
2020-04-04 18:00:55 -0700

    add by auto checkIn

--- nextJsLearn/src/pages/index.tsx
+++ nextJsLearn/src/pages/index.tsx
@@ -12,25 +12,6 @@ const Home: NextPage<HomeProps> = (props: HomeProps) => {
   return <React.Fragment></React.Fragment>;
 };
 
-// export const getStaticProps: GetStaticProps = async ctx => {
-//   console.log(ctx);
-
-//   return {
-//     props: {}
-//   };
-// };
-
-// export const getServerSideProps: GetServerSideProps = async ctx => {
-//   const country = ctx.query.country || 'us';
-//   process.browser ? Router.replace('/[country]', `/${country}`) : ctx.res.writeHead(302, { Location: `/${country}` });
-
-//   ctx.res.end();
-
-//   return {
-//     props: {}
-//   };
-// };
-
 Home.defaultProps = {};
 
 export default Home;
