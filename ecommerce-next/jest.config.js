55ac80ec9a35d9df81047d206e2c23ae97bb0676
Lei Zhao <lei.zhao@coupa.com>
2021-07-26 18:00:28 -0700

    add by auto checkIn

---
+++ ecommerce-next/jest.config.js
@@ -0,0 +1,14 @@
+const basicConfig = require('./jest.config.basic.js');
+
+module.exports = {
+  ...basicConfig,
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
+};
